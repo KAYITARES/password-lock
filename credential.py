33ecc23c633c8b9679dc4ddc5037e4afba82f741
MBOYIRE <mboyire14@gmail.com>
2019-02-11 16:57:37 +0200

    adding delete test in credential_text.py

--- credential.py
+++ credential.py
@@ -14,6 +14,14 @@ class Credential:
 
         Credential.credential_list.append(self)
 
+    def delete_credential(self):
+
+        '''
+        delete_credential method deletes a saved credential from the credential_list
+        '''
+
+        Credential.credential_list.remove(self)
+
 
     def __init__(self,account_name,password):
         '''
