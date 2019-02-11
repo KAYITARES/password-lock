df39fd5224c6a0129cc8076e330fa8a5c7a88c79
MBOYIRE <mboyire14@gmail.com>
2019-02-11 16:37:14 +0200

    adding teamDown in credential_text.py

--- credential_text.py
+++ credential_text.py
@@ -45,5 +45,23 @@ class TestCredential(unittest.TestCase):
         test_credential.save_credential()
         self.assertEqual(len(Credential.credential_list),2)
 
+    #setUp and class creation up here
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        Credential.credential_list = []
+
+    # other test cases here
+    def test_save_multiple_credential(self):
+        '''
+        test_save _multiple_credential to check if we can save multiple credential
+        objects to our credential_list
+        '''
+        self.new_credential.save_credential()
+        test_credential = Credential("Test","user") #new credential
+        test_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list),2)
+
 if __name__=='__main__':
         unittest.main()
