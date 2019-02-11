ae356fd985b4f4b2de0401ea99c7bc646d7b9cac
MBOYIRE <mboyire14@gmail.com>
2019-02-11 18:35:58 +0200

    creating create_user and credention function in run.py

--- run.py
+++ run.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3.6
+from user import User
+from credential import Credential
+
+def create_user(fname,lname,password,confirmation_password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(fname,lname,password,confirmation_password)
+    return new_user
+    
+def create_credential(account_name,password):
+    '''
+    Function to create a credential user
+    '''
+    new_credential = Credential(account_name,password)
+    return new_credential
